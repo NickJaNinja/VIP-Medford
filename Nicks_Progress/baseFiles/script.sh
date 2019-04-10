5429666e4df0fc0047f90e3c040184c3ecfd73c3
NickJaNinja <ndf99551@uga.edu>
2019-04-10 03:14:54 -0400

    script.sh is better

--- Nicks_Progress/baseFiles/script.sh
+++ Nicks_Progress/baseFiles/script.sh
@@ -1,3 +1,12 @@
 #job name: {NAME}
 #this script's location: {SCRIPT_LOCATION}
+#!/bin/bash
+#PBS -N {NAME}
+#PBS -l nodes=1:ppn=14
+#PBS -l walltime=5:00
+#PBS -q pace-ice
+#PBS -o output_dft
+
+source /nv/pace-ice/bcomer3/ug_esp_env.sh
+
 {COMMAND}
